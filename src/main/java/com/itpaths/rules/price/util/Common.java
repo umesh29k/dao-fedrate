a91ad84ef29459b179200428061b7655ec36db2b
rhdmtraining <Dilwil@99>
2020-10-15 20:13:30 +0000

    reflection removal

--- src/main/java/com/itpaths/rules/price/util/Common.java
+++ src/main/java/com/itpaths/rules/price/util/Common.java
@@ -1,40 +1,49 @@
 package com.itpaths.rules.price.util;
 
+import com.itpaths.rules.price.exception.ApiException;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Common {
-    public static double invokeForumla(String frml_id, Double[] E1, Double[] DS, Double[] param) throws InvocationTargetException {
+    public static double invokeForumla(String frml_id, Double[] E1, Double[] DS, Double[] param) throws InvocationTargetException, ApiException {
         double result = 0;
         //Method sumInstanceMethod;
         //try {
         //  sumInstanceMethod = FormulaUtil.class.getMethod(frml_id);
         FormulaUtil formula = new FormulaUtil(param, E1, DS);
+        if(frml_id != null) {
+            if (frml_id.equalsIgnoreCase("RT_CLASSIC")) {
+                result = (Double) formula.RT_CLASSIC();
+            } else if (frml_id.equalsIgnoreCase("RT_CLASSIC_DEBET")) {
+                result = (Double) formula.RT_CLASSIC_DEBET();
+            } else if (frml_id.equalsIgnoreCase("RT_CLASSIC_FULL")) {
+                result = (Double) formula.RT_CLASSIC_FULL();
+            } else if (frml_id.equalsIgnoreCase("RT_SUPLMNT")) {
+                result = (Double) formula.RT_SUPLMNT();
+            } else if (frml_id.equalsIgnoreCase("RT_CLASSIC_BASE")) {
+                result = (Double) formula.RT_CLASSIC_BASE();
+            } else if (frml_id.equalsIgnoreCase("RT_ALTRNV_PRICE")) {
+                result = (Double) formula.RT_ALTRNV_PRICE();
+            } else if (frml_id.equalsIgnoreCase("RT_VIA_PRICE")) {
+                result = (Double) formula.RT_VIA_PRICE();
+            } else if (frml_id.equalsIgnoreCase("RT_NORMAL_PRICE")) {
+                result = formula.RT_NORMAL_PRICE().doubleValue();
+            } else if (frml_id.equalsIgnoreCase("RT_TRIPS")) {
+                result = (Double) formula.RT_TRIPS();
+            } else if (frml_id.equalsIgnoreCase("RT_1STCLASS_DSTNC")) {
+                result = (Double) formula.RT_1STCLASS_DSTNC();
+            } else if (frml_id.equalsIgnoreCase("RT_MAX")) {
+                result = (Double) formula.RT_MAX();
+            } else if (frml_id.equalsIgnoreCase("RT_ITNRY")) {
+                result = (Double) formula.RT_ITNRY();
+            }
+            else
+                throw new ApiException("No formula id found");
 
-        if (frml_id.equalsIgnoreCase("RT_CLASSIC")) {
-            result = (Double) formula.RT_CLASSIC();
-        } else if (frml_id.equalsIgnoreCase("RT_CLASSIC_DEBET")) {
-            result = (Double) formula.RT_CLASSIC_DEBET();
-        } else if (frml_id.equalsIgnoreCase("RT_CLASSIC_FULL")) {
-            result = (Double) formula.RT_CLASSIC_FULL();
-        } else if (frml_id.equalsIgnoreCase("RT_SUPLMNT")) {
-            result = (Double) formula.RT_SUPLMNT();
-        } else if (frml_id.equalsIgnoreCase("RT_CLASSIC_BASE")) {
-            result = (Double) formula.RT_CLASSIC_BASE();
-        } else if (frml_id.equalsIgnoreCase("RT_ALTRNV_PRICE")) {
-            result = (Double) formula.RT_ALTRNV_PRICE();
-        } else if (frml_id.equalsIgnoreCase("RT_VIA_PRICE")) {
-            result = (Double) formula.RT_VIA_PRICE();
-        } else if (frml_id.equalsIgnoreCase("RT_NORMAL_PRICE")) {
-            result = formula.RT_NORMAL_PRICE().doubleValue();
-        } else if (frml_id.equalsIgnoreCase("RT_TRIPS")) {
-            result = (Double) formula.RT_TRIPS();
-        } else if (frml_id.equalsIgnoreCase("RT_1STCLASS_DSTNC")) {
-            result = (Double) formula.RT_1STCLASS_DSTNC();
-        } else if (frml_id.equalsIgnoreCase("RT_MAX")) {
-            result = (Double) formula.RT_MAX();
-        } else if (frml_id.equalsIgnoreCase("RT_ITNRY")) {
-            result = (Double) formula.RT_ITNRY();
+        }
+        else{
+            throw new ApiException("No formula id found");
         }
         //result = (Double) sumInstanceMethod.invoke(formula);
         /*} catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
--- src/main/java/com/itpaths/rules/price/util/Price_Natr_Id.java
+++ src/main/java/com/itpaths/rules/price/util/Price_Natr_Id.java
@@ -86,63 +86,66 @@ public class Price_Natr_Id {
         } catch (Exception e) {
             priceCode.setTrfPpFrmlId("SAS_DBF_TT_FORMULA");
         }
+        if (priceCode.getPriceNatrId() != null)
+            if (price_cd.get(priceCode.getPriceNatrId()) != null)
+                if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF")
+                        || price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_PLUS")
+                        || price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_MAX")) {
+                    if (priceRequest.getVoygr_id() != null)
+                        if (voygr_id.get(priceRequest.getVoygr_id()) != null)
+                            if (voygr_id.get(priceRequest.getVoygr_id()).equalsIgnoreCase("RTP_T_VY_ADULT")) {
+                                pcVoygr = pcVoygrRepo.findByVoygrId(priceRequest.getVoygr_id());
 
-        if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF")
-                || price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_PLUS")
-                || price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_MAX")) {
-            if (voygr_id.get(priceRequest.getVoygr_id()).equalsIgnoreCase("RTP_T_VY_ADULT")) {
-                pcVoygr = pcVoygrRepo.findByVoygrId(priceRequest.getVoygr_id());
-
-                params[6] = Double.parseDouble(pcVoygr.getPcRdctnCffcnt().toString());
-                if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_PLUS")) {
-                    params[21] = pcVoygr.getPcSuplmntAmtEur();
-                    params[23] = Double.parseDouble(pcVoygr.getPcUplftAmtEur().toString());
-                }
-                params[20] = Double.parseDouble("99999");
-                if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_MAX")) {
-                    try {
-                        PcVoygrClass pcVoygrClass = get_pc_voygr_class(priceRequest.getVoygr_id(),
-                                priceCode.getPriceCd(), 4,
-                                priceCode.getPcVrsn());
-                        params[20] = Double.valueOf(pcVoygrClass.getPcMaxAmtEur() + diabolo_amt_single);
-                    } catch (ApiException e) {
-                        e.printStackTrace();
-                    }
-                    params[27] = 0.0;
-                    tkt_price_eur = get_price_classic(priceRequest);
-                    qty = priceRequest.getQty();
-                    qty = qty - 1;
-                    if (qty > 0) {
-                        params[20] = (double) 99999;
-                        if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_MAX")) {
-                            String voygrId = null;
-                            for (String key : voygr_id.keySet()) {
-                                if (voygr_id.get(key).equalsIgnoreCase("RTP_T_VY_ADTNL_ADULT")) {
-                                    voygrId = key;
+                                params[6] = Double.parseDouble(pcVoygr.getPcRdctnCffcnt().toString());
+                                if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_PLUS")) {
+                                    params[21] = pcVoygr.getPcSuplmntAmtEur();
+                                    params[23] = Double.parseDouble(pcVoygr.getPcUplftAmtEur().toString());
+                                }
+                                params[20] = Double.parseDouble("99999");
+                                if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_MAX")) {
+                                    try {
+                                        PcVoygrClass pcVoygrClass = get_pc_voygr_class(priceRequest.getVoygr_id(),
+                                                priceCode.getPriceCd(), 4,
+                                                priceCode.getPcVrsn());
+                                        params[20] = Double.valueOf(pcVoygrClass.getPcMaxAmtEur() + diabolo_amt_single);
+                                    } catch (ApiException e) {
+                                        e.printStackTrace();
+                                    }
+                                    params[27] = 0.0;
+                                    tkt_price_eur = get_price_classic(priceRequest);
+                                    qty = priceRequest.getQty();
+                                    qty = qty - 1;
+                                    if (qty > 0) {
+                                        params[20] = (double) 99999;
+                                        if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_MAX")) {
+                                            String voygrId = null;
+                                            for (String key : voygr_id.keySet()) {
+                                                if (voygr_id.get(key).equalsIgnoreCase("RTP_T_VY_ADTNL_ADULT")) {
+                                                    voygrId = key;
+                                                }
+                                            }
+                                            try {
+                                                PcVoygrClass pcVoygrClass = get_pc_voygr_class(voygrId, priceCode.getPriceCd(), 4,
+                                                        priceCode.getPcVrsn());
+                                                params[20] = Double.valueOf(pcVoygrClass.getPcMaxAmtEur() + diabolo_amt_single);
+                                            } catch (ApiException e) {
+                                                e.printStackTrace();
+                                            }
+                                            PcVoygr pcVoygr = pcVoygrRepo.findByVoygrId(voygrId);
+                                            params[6] = Double.valueOf(pcVoygr.getPcRdctnCffcnt());
+                                        } else if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_PLUS")) {
+                                            params[21] = pcVoygr.getPcSuplmntAmtEur();
+                                            params[23] = (double) pcVoygr.getPcUplftAmtEur();
+                                        }
+                                        tkt_price_eur = get_price_classic(priceRequest);
+                                    }
                                 }
                             }
-                            try {
-                                PcVoygrClass pcVoygrClass = get_pc_voygr_class(voygrId, priceCode.getPriceCd(), 4,
-                                        priceCode.getPcVrsn());
-                                params[20] = Double.valueOf(pcVoygrClass.getPcMaxAmtEur() + diabolo_amt_single);
-                            } catch (ApiException e) {
-                                e.printStackTrace();
-                            }
-                            PcVoygr pcVoygr = pcVoygrRepo.findByVoygrId(voygrId);
-                            params[6] = Double.valueOf(pcVoygr.getPcRdctnCffcnt());
-                        } else if (price_cd.get(priceCode.getPriceNatrId()).equalsIgnoreCase("RTP_T_PN_MTARIFF_PLUS")) {
-                            params[21] = pcVoygr.getPcSuplmntAmtEur();
-                            params[23] = (double) pcVoygr.getPcUplftAmtEur();
-                        }
-                        tkt_price_eur = get_price_classic(priceRequest);
-                    }
+                } else {
+                    PcVoygr pcVoygr = pcVoygrRepo.findByVoygrId(priceRequest.getVoygr_id());
+                    params[6] = Double.valueOf(pcVoygr.getPcRdctnCffcnt());
+                    tkt_price_eur = get_price_classic(priceRequest);
                 }
-            }
-        } else {
-            PcVoygr pcVoygr = pcVoygrRepo.findByVoygrId(priceRequest.getVoygr_id());
-            params[6] = Double.valueOf(pcVoygr.getPcRdctnCffcnt());
-            tkt_price_eur = get_price_classic(priceRequest);
-        }
         pr.setTotal_price_eur(tkt_price_eur);
         return pr;
     }
@@ -523,7 +526,7 @@ public class Price_Natr_Id {
             else
                 tkt_price_eur = pcVoygr.getPcSuplmntAmtEur();
         } else
-            tkt_price_eur =  do_classic(priceRequest).getTotal_price_eur();
+            tkt_price_eur = do_classic(priceRequest).getTotal_price_eur();
         //tkt_price_eur += do_classic_plus(priceRequest);
         pr.setTotal_price_eur(tkt_price_eur);
         return pr;
